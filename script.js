f2c88b1f32b5849b11e64bd5916f37df83c06402
smasa1112 <mtsakagami1112@yahoo.co.jp>
2022-07-22 00:58:20 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+  
+    });
+
+    $(".hold-on-move").click(function() {
+        var target_id_name=('#'+($(this).attr('id').substr(5)));
+        console.log(target_id_name)
+        console.log(typeof ($(this).attr('id')));
+        var position = $(target_id_name).offset().top;
+        const speed = 500;
+        $("html,body").animate({scrollTop:position},speed);
+    })
+  });
